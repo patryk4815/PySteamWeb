cd43c186f488502970264423a344aaeb0cd693c7
Patryk Sondej <patryk.sondej@gmail.com>
2015-11-08 20:03:12 +0100

    fix version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='PySteamWeb',
-    version='0.2',
+    version='0.2.2',
     packages=['pysteamweb'],
     url='https://github.com/patryk4815/PySteamWeb',
     license='MIT',
